0d8962ee03dd36489b22274865eb06dc30c47404
Leônidas Luigy <52331780+leoLUIGY@users.noreply.github.com>
2025-03-20 13:13:34 +0000

    finish `/api/v1/status` endpoint

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c82345f205a49a6e"; }
+/******/ 		__webpack_require__.h = function() { return "c187cf70a85181ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
