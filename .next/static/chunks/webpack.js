eaebf3aa97e29070283020b8e00ccc3302154158
Leônidas Luigy <52331780+leoLUIGY@users.noreply.github.com>
2025-03-22 18:20:13 +0000

    handles database ssl connection

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "065f7c54d918fea6"; }
+/******/ 		__webpack_require__.h = function() { return "0d5b4210e4fd52e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
